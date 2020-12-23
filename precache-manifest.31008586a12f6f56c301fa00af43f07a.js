f01fc81fd753d47e8a67e55b71c8a56f5ee7556b
Pavel Leonov <pavel.leonov@aviasales.ru>
2020-12-23 18:36:28 +0300

    Updates

---
+++ precache-manifest.31008586a12f6f56c301fa00af43f07a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c3d0ef1a8c30a1c0e99fcfd57549f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa90d69afe8939bbad49",
+    "url": "./static/css/main.07a6e54e.chunk.css"
+  },
+  {
+    "revision": "39e02b99d360fd48bbdb",
+    "url": "./static/js/2.44c1e756.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.44c1e756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa90d69afe8939bbad49",
+    "url": "./static/js/main.3fb732c9.chunk.js"
+  },
+  {
+    "revision": "410b701a1330f9535b01",
+    "url": "./static/js/runtime-main.3d0d29fc.js"
+  },
+  {
+    "revision": "52d79d74e3f92e47ddaa1f33ffe8a87a",
+    "url": "./static/media/element_02_lq.52d79d74.mp4"
+  },
+  {
+    "revision": "62f4cd70d499ecbe392e2de201c5c329",
+    "url": "./static/media/space_bg.62f4cd70.mp4"
+  },
+  {
+    "revision": "ea0690b189f5eebbd3f26614545e7fd1",
+    "url": "./static/media/video_with_mask.ea0690b1.mp4"
+  }
+]);
